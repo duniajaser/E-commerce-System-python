bf26ce750e2eb6eaec896488d19e0ce12804c1d9
duniajaser <153684727+duniajaser@users.noreply.github.com>
2024-03-26 19:01:44 +0200

    User.py

---
+++ User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, id_num, username, date_of_birth, active):
+        self.username = username
+        self.id_num = id_num
+        self.date_of_birth = date_of_birth
+        self.active = active
